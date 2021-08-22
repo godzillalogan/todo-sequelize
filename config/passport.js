d7eb0500690f5cfa07bd6ab8d812bcc344709f06
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-08-22 20:33:37 +0800

    Flash message：給使用者提示

--- config/passport.js
+++ config/passport.js
@@ -7,25 +7,29 @@ const db = require('../models')
 const User = db.User
 
 module.exports = app => {
+  // 初始化 Passport 模組
   app.use(passport.initialize())
   app.use(passport.session())
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ where: { email } })  //查詢特定email的User
-      .then(user => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
-        }
-        return bcrypt.compare(password, user.password).then(isMatch => {
-          if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
+  // 設定本地登入策略
+  passport.use(new LocalStrategy({ 
+    usernameField: 'email', passwordField: 'password', passReqToCallback: true, session: false }, 
+    (req, email, password, done) => {
+      User.findOne({ where: { email } })  //查詢特定email的User
+        .then(user => {
+          if (!user) {
+            return done(null, false, req.flash('warning_msg', '這個Email還沒註冊!'))
           }
-          return done(null, user)
+          return bcrypt.compare(password, user.password).then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, req.flash('warning_msg', 'Email或Password不正確。'))
+            }
+            return done(null, user)
+          })
         })
-      })
-      .catch(err => done(err, false))
-  }))
-  passport.serializeUser((user, done) => {
-    done(null, user.id)
+        .catch(err => done(err, false))
+    }))
+    passport.serializeUser((user, done) => {
+      done(null, user.id)
   })
   //在做 passport.deserializeUser 的時候，由於這筆 User 物件常常會透過 req.user 傳到前端樣板，這裡要先轉成 plain object。
   passport.deserializeUser((id, done) => {
